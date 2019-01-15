6f71fa3a20f524c32587117c10cf8ab491874abf
Roland Crosby <roland@cockroachlabs.com>
2019-01-15 13:46:11 -0500

    package it up

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pasteboard',
+  install_requires=['pyobjc'],
+  packages=['pasteboard'],
+  description='inspect and manipulate macOS pasteboard',
+  version='1.0',
+  url='https://github.com/rolandcrosby/pasteboard',
+  author='Roland Crosby',
+  author_email='roland@rolandcrosby.com',
+  license='ISC',
+  scripts=['bin/pb'])
